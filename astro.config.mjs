6852b9931f204e67cccbf074ab7704b09a12aab8
ethantphillips <marktphillipsmeo@gmail.com>
2025-05-31 13:29:20 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8" />
-  <title>Netlify CMS</title>
-  <script>
-    if (sessionStorage.getItem('cmsPin') !== 'granted') {
-      window.location.href = '/admin-auth/';
-    }
-  </script>
-  <script src="https://unpkg.com/netlify-cms@^2.10.0/dist/netlify-cms.js"></script>
-</head>
-<body>
-  <div id="nc-root"></div>
-  <script>
-    if (typeof CMS !== 'undefined') {
-      CMS.init();
-    } else {
-      console.error('CMS failed to load.');
-    }
-  </script>
-</body>
-</html>
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+
+export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+});

bd5d2c63c81c2d08f7fc2552a245ee6131520752
ethantphillips <marktphillipsmeo@gmail.com>
2025-06-05 09:31:37 -0400

    i3ioteqoignvs

--- dist/admin/__env.js
+++
@@ -1,8 +0,0 @@
-
-window.__env = {
-  ADMIN_PIN_1: "%ADMIN_PIN_1%",
-  ADMIN_PIN_2: "%ADMIN_PIN_2%",
-  ADMIN_PIN_3: "%ADMIN_PIN_3%",
-  ADMIN_PIN_4: "%ADMIN_PIN_4%",
-  ADMIN_PIN_5: "%ADMIN_PIN_5%"
-};
--- dist/admin/scripts/pin-check.js
+++
@@ -1,8 +0,0 @@
-export function getValidPins() {
-  const allowed = [];
-  for (let i = 1; i <= 10; i++) {
-    const pin = import.meta.env[`PUBLIC_ADMIN_PIN_${i}`];
-    if (pin) allowed.push(pin);
-  }
-  return allowed;
-}
